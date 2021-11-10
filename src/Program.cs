e28f65899664d3b24e350bebac848f6762278e00
ksheerasagar17 <ksheerasagar17@gmail.com>
2021-11-09 23:34:54 -0600

    adding sum of all digits code

---
+++ src/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace sumofdigitsis3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var from = 1200;
+            var to = 1259;
+            var increment = 1;
+            StartPrinting(from, to, increment);
+        }
+
+        static private void StartPrinting(int from, int to, int increment)
+        {
+            for (int i = from; i <= to; i = (i + increment))
+            {
+                var sumOfAlldigits = SumUpAllDigits(i);
+                if (sumOfAlldigits == 3)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static private int SumUpAllDigits(int inputNumber)
+        {
+            var summation = 0;
+            while (inputNumber > 0)
+            {
+                summation += inputNumber % 10;
+                inputNumber /= 10;
+            }
+
+            if (summation > 9)
+            {
+                summation = SumUpAllDigits(summation);
+            }
+
+            return summation;
+        }
+    }
+}
